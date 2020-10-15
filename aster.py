02f28c809ab59158f95a5f710d22f897c01e0a28
Alex Sidorenko <alex.sidore80@gmail.com>
2020-10-15 18:40:58 +0300

    tasks

---
+++ aster.py
@@ -0,0 +1,7 @@
+a = 0
+for i in "Even though Israel is a small country it is always in the news.  The news continues to report on Jews moving to Israel, on the technology invented there, but also on conflict, wars and tensions with surrounding people.":
+    if i == "a":
+        a += 1
+
+print(a)
+
